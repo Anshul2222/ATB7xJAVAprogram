b5b12f9c4b53490d2b374f0a67d6f8ee260dfdbc
anshul2222 <khannaanshul72@gmail.com>
2024-07-26 13:08:07 +0530

    Initial commit

---
+++ src/ex_07062024/Lab003.java
@@ -0,0 +1,19 @@
+package ex_07062024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        // single comment - this code will not run
+
+        //purpose comment
+        // instruction to Jr QA or Dev
+        //explain
+        /*
+
+        Multiple comment
+        Documentation purpose
+
+
+         */
+
+    }
+}
---
+++ src/ex_07062024/Lab004.java
@@ -0,0 +1,12 @@
+package ex_07062024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        // package ->class -> amin method ->statements(instruction)
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+       System.out.println(2-2);
+        System.out.println(3/2); // Integer
+            }
+}
